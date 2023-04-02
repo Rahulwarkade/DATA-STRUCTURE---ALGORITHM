4032ec504b088469e40ae42f30eb83e1d8cb62d2
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-02 23:26:40 +0530

    Recursion

--- Recursion.java
+++ Recursion.java
@@ -111,7 +111,7 @@ public class Recursion
 		ob.decreasingOrder(10);
 		// ob.increasingOrder(10);
 		System.out.println(ob.factorial(5));
-		// System.out.println(ob.sumOfNnum(10));
+		System.out.println(ob.sumOfNnum(10));
 		System.out.println(ob.fibonacci(6));
 		int arr[] = {8,3,6,9,5,10,3,5,3};
 		// System.out.println(ob.isSorted(arr,0));
