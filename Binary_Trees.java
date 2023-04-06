6e6037a5c8b9424117cf2e81487e50a5e14ca04c
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-06 17:53:31 +0530

    Diameter

--- Binary_Trees.java
+++ Binary_Trees.java
@@ -144,9 +144,9 @@ public class Binary_Trees
 	}
 	public static void main(String args[])
 	{
-		int node[] = {1,2,4,-1,-1,5,-1,6,-1,7,-1,-1,3,-1,-1};
+		// int node[] = {1,2,4,-1,-1,5,-1,6,-1,7,-1,-1,3,-1,-1};
 
-		// int node[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+		int node[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
 
 		BTree tree = new BTree();
 		Node root = tree.buildTree(node);
