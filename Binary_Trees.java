711107b7b0adf549b5a80eb2ef31e6acd356c3e4
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-06 14:24:57 +0530

    InOrder and PostOrder Traversal

--- Binary_Trees.java
+++ Binary_Trees.java
@@ -34,12 +34,32 @@ public class Binary_Trees
 
 	public static void preOrder(Node root)
 	{
-		if(root == null)
+		if(root == null){
+			System.out.print("-1 ");
 			return;
+		}
 
 		System.out.print(root.data+" ");
 		preOrder(root.left);
 		preOrder(root.right);
+	}
+
+	public static void inOrder(Node root)
+	{
+		if(root==null)
+			return;
+		inOrder(root.left);
+		System.out.print(root.data+" ");
+		inOrder(root.right);
+	}
+
+	public static void postOrder(Node root)
+	{
+		if(root ==null)
+			return;
+		postOrder(root.left);
+		postOrder(root.right);
+		System.out.print(root.data+" ");
 	}
 	public static void main(String args[])
 	{
@@ -54,6 +74,6 @@ public class Binary_Trees
 		// System.out.println(root.right.data);
 		// System.out.println(root.right.right.data);
 
-		preOrder(root);
+		postOrder(root);
 	}
 }
