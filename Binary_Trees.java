ed0499bc159084866909a3cfbbcf90726ceea3db
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-06 14:04:07 +0530

    PreOrder Traversal

--- Binary_Trees.java
+++ Binary_Trees.java
@@ -31,17 +31,29 @@ public class Binary_Trees
 			return newNode;
 		}
 	}
+
+	public static void preOrder(Node root)
+	{
+		if(root == null)
+			return;
+
+		System.out.print(root.data+" ");
+		preOrder(root.left);
+		preOrder(root.right);
+	}
 	public static void main(String args[])
 	{
 		int node[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
 
 		BTree tree = new BTree();
 		Node root = tree.buildTree(node);
-		System.out.println(root.data);
-		System.out.println(root.left.data);
-		System.out.println(root.left.left.data);
-		System.out.println(root.left.right.data);
-		System.out.println(root.right.data);
-		System.out.println(root.right.right.data);
+		// System.out.println(root.data);
+		// System.out.println(root.left.data);
+		// System.out.println(root.left.left.data);
+		// System.out.println(root.left.right.data);
+		// System.out.println(root.right.data);
+		// System.out.println(root.right.right.data);
+
+		preOrder(root);
 	}
 }
