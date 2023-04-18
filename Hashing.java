f83e585f88c97a28cc8c364f4fb9a6fae3e950f3
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-18 17:16:17 +0530

    HashMap

--- Hashing.java
+++ Hashing.java
@@ -6,11 +6,30 @@ public class Hashing
 	{
 		HashMap<String,Integer> hm = new HashMap<>();
 
+// .put(key,value) -> O(1)
 		hm.put("India",130);
 		hm.put("China",120);
 		hm.put("US",30);
 		hm.put("Russia",100);
 
 		System.out.println(hm);
+// .get(key) -> O(1)
+		int population = hm.get("India");
+		System.out.println("India population = "+population);
+		System.out.println("Indonesia popultaion = "+hm.get("Indonesia"));
+
+// .contiansKey(key) -> O(1)
+
+		System.out.println(hm.containsKey("US"));
+		System.out.println(hm.containsKey("UK"));
+// .remove(key) -> O(1)
+
+		System.out.println(hm.remove("China"));
+		System.out.println(hm);
+
+		System.out.println(hm.size());
+		System.out.println(hm.isEmpty());
+		hm.clear();
+		System.out.println(hm.isEmpty());
 	}
 }
