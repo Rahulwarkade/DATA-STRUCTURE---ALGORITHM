c97873d8a66ba8897ec2d87137acad4e166e27a6
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-18 17:03:12 +0530

    Hashing

---
+++ Hashing.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Hashing
+{
+	public static void main(String args[])
+	{
+		HashMap<String,Integer> hm = new HashMap<>();
+
+		hm.put("India",130);
+		hm.put("China",120);
+		hm.put("US",30);
+		hm.put("Russia",100);
+
+		System.out.println(hm);
+	}
+}
