c63bd354e4a02496a7ec670c4d7ece5eb57abaa2
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-18 06:52:52 +0530

    N Ropes with Minimum cost

--- Heaps.java
+++ Heaps.java
@@ -234,5 +234,24 @@ public class Heaps
 		// {
 		// 	System.out.println("C"+p.remove().idx);
 		// }
+
+		int ropes[] = {4,3,3,2,6};
+		PriorityQueue<Integer> rop = new PriorityQueue<>();
+
+		for(int i=0; i<ropes.length; i++)
+		{
+			rop.add(ropes[i]);
+		}
+
+		int ans = 0;
+
+		for(int i=0; i<ropes.length-1; i++)
+		{
+			int num1 = rop.remove();
+			int num2 = rop.remove();
+			ans += num1+num2;
+			rop.add(num1+num2);
+		}
+		System.out.println(ans);
 	}
 }
