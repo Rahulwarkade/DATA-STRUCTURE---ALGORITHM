c7a9aa473cf72967ccb592b9d706e8fa4a942e9b
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-19 07:52:50 +0530

    LinkedHashMap

--- Hashing.java
+++ Hashing.java
@@ -3,68 +3,163 @@ import java.util.*;
 public class Hashing
 {
 
-	public static class HashMap<K,V>
-	{
-		class Node 
-		{
-			K key;
-			V value;
+	// public static class HashMap<K,V>
+	// {
+	// 	class Node 
+	// 	{
+	// 		K key;
+	// 		V value;
 
-			Node(K key, V value)
-			{
-				this.key = key;
-				this.value = value;
-			}
-		}
+	// 		Node(K key, V value)
+	// 		{
+	// 			this.key = key;
+	// 			this.value = value;
+	// 		}
+	// 	}
 
-		private  int n;
-		private  int N;
-		private LinkedList<Node> buckets[];
+	// 	private int n;
+	// 	private int N;
+	// 	private LinkedList<Node> buckets[];
 
-		@SuppressWarnings("unchecked")
-		HashMap()
-		{
-			this.N = 4;
-			this.n = 0;
-			this.buckets = new LinkedList[4];
-			for(int i=0; i<N; i++)
-				buckets[i] = new LinkedList<Node>();
-		}
+	// 	@SuppressWarnings("unchecked")
+	// 	HashMap()
+	// 	{
+	// 		this.N = 4;
+	// 		this.n = 0;
+	// 		this.buckets = new LinkedList[4];
+	// 		for(int i=0; i<N; i++)
+	// 			buckets[i] = new LinkedList<Node>();
+	// 	}
 
-		private int hashFun(K key)
-		{
-			int idx = key.hashCode();
-			return Math.abs(idx)%N;
-		}
-		private int searchInLL(int dIdx,K key)
-		{
-			LinkedList<Node> ll = buckets[dIdx];
-			int d = 0;
-			for(int i=0; i<ll.size(); i++)
-			{
-				if(ll.get(i)==key) return d;
-				else d++;
-			}
-			return -1;
-		}
-		public void put(K key,V value)
-		{
-			int bIdx = hashFun(key);
-			int dIdx = searchInLL(bIdx,key);
+	// 	private int hashFun(K key)
+	// 	{
+	// 		int idx = key.hashCode();
+	// 		return Math.abs(idx)%N;
+	// 	}
+	// 	private int searchInLL(int dIdx,K key)
+	// 	{
+	// 		LinkedList<Node> ll = buckets[dIdx];
+	// 		int d = 0;
+	// 		for(int i=0; i<ll.size(); i++)
+	// 		{
+	// 			if(ll.get(i).key==key) return d;
+	// 			else d++;
+	// 		}
+	// 		return -1;
+	// 	}
 
-			if(dIdx!=-1)
-			{
-				Node node = buckets[bIdx].get(dIdx);
-				node.value = value;
-			}
-			else 
-			{
-				Node newNode = new Node(key,value);
-				buckets[bIdx].add(newNode);
-				n++;
-			}
-		}
-	}
+    //     @SuppressWarnings("unchecked")
+    //     private void rehash()
+    //     {
+    //         LinkedList<Node> oldBuckets[] = buckets;
+    //         buckets = new LinkedList[2*N];
+    //         N = 2*N;
+    //         n = 0;
+    //         for(int i=0; i<N; i++)
+    //         {
+    //             buckets[i] = new LinkedList<>();
+    //         }
+
+    //         for(int i=0; i<oldBuckets.length; i++)
+    //         {
+    //             LinkedList<Node> ll = oldBuckets[i];
+    //             for(int j=0; j<ll.size(); j++)
+    //             {
+    //                 Node node = ll.get(j);
+    //                 put(node.key,node.value);
+    //             }
+    //         }
+    //     }
+
+    //     public boolean isEmpty()
+    //     {
+    //         return n==0;
+    //     }
+	// 	public void put(K key,V value)
+	// 	{
+	// 		int bIdx = hashFun(key);
+	// 		int dIdx = searchInLL(bIdx,key);
+
+
+	// 		if(dIdx!=-1)
+	// 		{
+	// 			Node node = buckets[bIdx].get(dIdx);
+	// 			node.value = value;
+	// 		}
+	// 		else 
+	// 		{
+	// 			Node newNode = new Node(key,value);
+	// 			buckets[bIdx].add(newNode);
+	// 			n++;
+	// 		}
+    //         double lambda = (double)n/N;
+    //         if(lambda>=1.0)
+    //         {
+    //             rehash();
+    //         }
+
+	// 	}
+
+    //     public boolean containsKey(K key)
+    //     {
+    //         int bIdx = hashFun(key);
+	// 		int dIdx = searchInLL(bIdx,key);
+	// 		if(dIdx!=-1)
+	// 		{
+    //             return true;
+	// 		}
+    //         return false;
+    //     }
+
+    //     public V get(K key)
+    //     {
+    //         int bIdx = hashFun(key);
+    //         int dIdx = searchInLL(bIdx, key);
+
+    //         if(dIdx!=-1)
+    //         {
+    //             Node node = buckets[bIdx].get(dIdx);
+    //             return node.value;
+    //         }
+    //             return null;
+    //     }
+
+    //     public V remove(K key)
+    //     {
+    //         int bIdx = hashFun(key);
+    //         int dIdx = searchInLL(bIdx, key);
+
+    //         if(dIdx!=-1)
+    //         {
+    //             Node node = buckets[bIdx].remove(dIdx);
+    //             n--;
+    //             return node.value;
+    //         }
+    //         return null;            
+    //     }
+
+    //     public ArrayList<K> keySet()
+    //     {
+    //         ArrayList<K> list = new ArrayList<>();
+    //         for(int i=0; i<N; i++)
+    //         {
+    //             LinkedList<Node> ll = buckets[i];
+    //             for(int j=0; j<ll.size(); j++)
+    //             {
+    //                 list.add(ll.get(j).key);
+    //             }
+    //         }
+    //         return list;
+    //     }
+    //     public int size()
+    //     {
+    //         return n;
+    //     }
+    //     public int length()
+    //     {
+    //         return N;
+    //     }
+	// }
 	public static void main(String args[])
 	{
 		// HashMap<String,Integer> hm = new HashMap<>();
@@ -103,9 +198,49 @@ public class Hashing
 		// 	System.out.println("key="+key+", value="+hm.get(key));
 		// }
 
+// Implementation of HashMap
+
+		// HashMap<String,Integer> hm = new HashMap<>();
+
+		// hm.put("India",24);
+		// hm.put("China",20);
+		// hm.put("US",10);
+		// hm.put("UK",5);
+		// hm.put("France",50);
+		// hm.put("Russia",99);
+		// hm.put("Japan",88);
+		// hm.put("Ukrain",88);
+		// hm.put("Argentina",88);
+		// hm.put("Pakistan",88);
+		// hm.put("Koria",88);
+		// hm.put("Astrilia",88);
+        
+        // ArrayList<String> keys = hm.keySet();
+        
+        // for (String key : keys) {
+        //     System.out.println("key = "+key+", value = "+hm.get(key));
+        // }
+
+        // System.out.println(hm.get("China"));
+        // System.out.println(hm.remove("China"));
+        // System.out.println(hm.get("China"));
+        // System.out.println(hm.isEmpty());
+        // System.out.println(hm.containsKey("India"));
+        // System.out.println(hm.size());
+        // System.out.println(hm.length());
+
+// LinkedHashMap
 		HashMap<String,Integer> hm = new HashMap<>();
+		LinkedHashMap<String,Integer> lhm = new LinkedHashMap<>();
+		hm.put("Ukrain",88);
+		hm.put("Argentina",88);
+		hm.put("Pakistan",88);
 
-		hm.put("India",24);
+		lhm.put("Ukrain",88);
+		lhm.put("Argentina",88);
+		lhm.put("Pakistan",88);
 
+		System.out.println(hm);
+		System.out.println(lhm);
 	}
 }
