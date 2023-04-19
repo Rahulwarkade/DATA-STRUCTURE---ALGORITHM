7b52e7dce4221e8f450504ea7e9972e4bd0bf0ef
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-19 07:32:07 +0530

    Hashing implimantation

--- Hashing.java
+++ Hashing.java
@@ -2,15 +2,78 @@ import java.util.*;
 
 public class Hashing
 {
+
+	public static class HashMap<K,V>
+	{
+		class Node 
+		{
+			K key;
+			V value;
+
+			Node(K key, V value)
+			{
+				this.key = key;
+				this.value = value;
+			}
+		}
+
+		private  int n;
+		private  int N;
+		private LinkedList<Node> buckets[];
+
+		@SuppressWarnings("unchecked")
+		HashMap()
+		{
+			this.N = 4;
+			this.n = 0;
+			this.buckets = new LinkedList[4];
+			for(int i=0; i<N; i++)
+				buckets[i] = new LinkedList<Node>();
+		}
+
+		private int hashFun(K key)
+		{
+			int idx = key.hashCode();
+			return Math.abs(idx)%N;
+		}
+		private int searchInLL(int dIdx,K key)
+		{
+			LinkedList<Node> ll = buckets[dIdx];
+			int d = 0;
+			for(int i=0; i<ll.size(); i++)
+			{
+				if(ll.get(i)==key) return d;
+				else d++;
+			}
+			return -1;
+		}
+		public void put(K key,V value)
+		{
+			int bIdx = hashFun(key);
+			int dIdx = searchInLL(bIdx,key);
+
+			if(dIdx!=-1)
+			{
+				Node node = buckets[bIdx].get(dIdx);
+				node.value = value;
+			}
+			else 
+			{
+				Node newNode = new Node(key,value);
+				buckets[bIdx].add(newNode);
+				n++;
+			}
+		}
+	}
 	public static void main(String args[])
 	{
-		HashMap<String,Integer> hm = new HashMap<>();
+		// HashMap<String,Integer> hm = new HashMap<>();
 
 // .put(key,value) -> O(1)
-		hm.put("India",130);
-		hm.put("China",120);
-		hm.put("US",30);
-		hm.put("Russia",100);
+		// hm.put("India",130);
+		// hm.put("China",120);
+		// hm.put("US",30);
+		// hm.put("Russia",100);
 
 		// System.out.println(hm);
 // .get(key) -> O(1)
@@ -32,13 +95,17 @@ public class Hashing
 		// hm.clear();
 		// System.out.println(hm.isEmpty());
 
-		Set<String> keys = hm.keySet();
+		// Set<String> keys = hm.keySet();
 
 		// System.out.println(hm.entrySet());
-		for(String key : keys)
-		{
-			System.out.println("key="+key+", value="+hm.get(key));
-		}
+		// for(String key : keys)
+		// {
+		// 	System.out.println("key="+key+", value="+hm.get(key));
+		// }
+
+		HashMap<String,Integer> hm = new HashMap<>();
+
+		hm.put("India",24);
 
 	}
 }
