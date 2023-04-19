041cbcf2ea658bebe44bf1b416cf4634cf83f8c6
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-19 13:34:33 +0530

    Itinerary

--- Hashing.java
+++ Hashing.java
@@ -259,6 +259,39 @@ public class Hashing
 
 		System.out.println("Intersection = "+set2);
 	}
+
+	public static String getStart(HashMap<String,String> tickets)
+	{
+		HashMap<String,String> revMap = new HashMap<String,String>();
+
+		for (String key : tickets.keySet()) {
+			revMap.put(tickets.get(key),key);
+		}
+
+		for (String key : tickets.keySet()) {
+			if(!revMap.containsKey(key))
+			{
+				return key;
+			}
+		}
+		return "";
+	}
+	public static void Itinerary()
+	{
+		HashMap<String,String> tickets = new HashMap<>();
+		tickets.put("Chennai","Bengaluru");
+		tickets.put("Mumbai","Delhi");
+		tickets.put("Goa","Chennai");
+		tickets.put("Delhi","Goa");
+
+		String start = getStart(tickets);
+
+		System.out.print(start);
+		for (String key : tickets.keySet()) {
+			System.out.print("->"+tickets.get(start));
+			start = tickets.get(start);
+		}
+	}
 	public static void main(String args[])
 	{
 		
@@ -422,6 +455,8 @@ public class Hashing
 
 		int[] num1 = {2,3,9,87,7,5,6};
 		int[] num2 = {2,9,47,7,8,2};
-		union_Intersection(num1,num2);
+		// union_Intersection(num1,num2);
+
+		Itinerary();
 	}
 }
