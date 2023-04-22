cbcad4648dab4927ecc83e4bbe7f204d84a06788
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-23 01:44:30 +0530

    Bellman ford

--- Graphs.java
+++ Graphs.java
@@ -379,6 +379,46 @@ public class Graphs
 			System.out.println(source+" -> "+i+" = "+dist[i]);
 		}
 	}
+
+	public static void bellmanFord(ArrayList<Edge>[] graph,int source)
+	{
+		int[] dist = new int[graph.length];
+
+		for(int i=0; i<graph.length; i++)
+		{
+			if(i!=source)
+			{
+				dist[i] = Integer.MAX_VALUE;
+			}
+		}
+
+		int V = graph.length;
+
+		for(int i=0; i<V-1; i++)
+		{
+			for(int j=0; j<graph.length; j++)
+			{
+				for(int k= 0; k<graph[j].size(); k++)
+				{
+					Edge e = graph[j].get(k);
+
+					int u = e.src;
+					int v = e.dest;
+					int wt = e.weight;
+
+					if(dist[u]!=Integer.MAX_VALUE && dist[u]+wt<dist[v])
+					{
+						dist[v] = dist[u]+wt;
+					}
+				}
+			}
+		}
+
+		for(int i=0; i<graph.length; i++)
+		{
+			System.out.println(dist[i]+ " ");
+		}
+	}
 	public static void main(String args[])
 	{
 		Scanner jin = new Scanner(System.in);
