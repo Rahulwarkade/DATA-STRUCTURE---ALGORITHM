8ba2cf515101dd19c26145ac28dbac5bd48fb593
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-05-04 22:59:49 +0530

    DP

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -5,12 +5,12 @@ public class DynamicProgramming
 
 	public static int fib(int n)
 	{
-		if(n==1 || n== 0) return 1;
+		if(n==1 || n== 0) return n;
 
 		return fib(n-1) + fib(n-2);
 	}
 	public static void main(String args[])
 	{
-		System.out.println(fib(4));
+		System.out.println(fib(5));
 	}
 }
