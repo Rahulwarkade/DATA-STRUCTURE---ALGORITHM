dbf36595c5aa469688c03c6c941834ed6ed2d53e
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-05-05 17:00:25 +0530

    Memoization in Fibonacci

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -1,16 +1,33 @@
 import java.util.*;
+import java.util.Arrays;
 
 public class DynamicProgramming
 {
 
-	public static int fib(int n)
+	public static int fib(int n,int[] dp)
 	{
 		if(n==1 || n== 0) return n;
-
-		return fib(n-1) + fib(n-2);
+		if(dp[n]!=-1)
+			return dp[n];
+		dp[n] = fib(n-1,dp) + fib(n-2,dp);
+		return dp[n];
+	}
+	public static void printDp(int[] dp)
+	{
+		for(int i=0; i<dp.length; ++i)
+			System.out.print(dp[i]+" ");
+		System.out.println();
 	}
 	public static void main(String args[])
 	{
-		System.out.println(fib(5));
+		int n;
+		Scanner jin = new Scanner(System.in);
+		n = jin.nextInt();
+		int dpfib[] = new int[n+1];
+		Arrays.fill(dpfib,-1);
+		dpfib[0] = 0;
+		dpfib[1] = 1;
+		System.out.println(fib(n,dpfib));
+		printDp(dpfib);
 	}
 }
