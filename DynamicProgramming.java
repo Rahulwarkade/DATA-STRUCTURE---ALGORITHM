d6fe0acb976f9a77807e7087711b8087ad9d3c5b
Rahul <88571832+Rahulwarkade@users.noreply.github.com>
2023-06-08 08:58:43 +0530

    lcs Tabulation

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -341,7 +341,7 @@ public class DynamicProgramming
 			return Math.max(lcs1,lcs2);
 		}
 	}
-	
+
 	public static int lcsMem(String str1,String str2,int n,int m,int[][] dp)
 	{
 		if(n<0||m<0) return 0;
@@ -360,6 +360,30 @@ public class DynamicProgramming
 
 		return dp[n][m];
 	}
+
+	public static int lcsTab(String str1,String str2,int n,int m)
+	{
+		int [][] dp = new int[n+1][m+1];
+
+		for(int i=1; i<=n; i++)
+		{
+			for(int j=1; j<=m; j++)
+			{
+				if(str1.charAt(i-1)==str2.charAt(j-1))
+				{
+					dp[i][j] = dp[i-1][j-1] + 1;
+				}
+				else 
+				{
+					int lcs1 = dp[i-1][j];
+					int lcs2 = dp[i][j-1];
+					dp[i][j] = Math.max(lcs1,lcs2);
+				}
+			}
+		}
+
+		return dp[n][m];
+	}
 	public static void longestCommonSubsequence()
 	{
 		String str1 = "abcde", str2 = "ace";
@@ -371,7 +395,8 @@ public class DynamicProgramming
 			Arrays.fill(dp[i],-1);
 		}
 		// System.out.println("lcs = "+lcsRec(str1,str2,n-1,m-1));
-		System.out.println("lcs = "+lcsMem(str1,str2,n-1,m-1,dp));
+		// System.out.println("lcs = "+lcsMem(str1,str2,n-1,m-1,dp));
+		System.out.println("lcs = "+lcsTab(str1,str2,n,m));
 	}
 	public static void main(String args[])
 	{
