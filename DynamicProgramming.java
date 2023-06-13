d95ea7e2620823db03956e0c2985ef78423ab69a
Rahul <88571832+Rahulwarkade@users.noreply.github.com>
2023-06-13 14:34:25 +0530

    Matrix Chain Multiplication

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -629,6 +629,30 @@ public class DynamicProgramming
 		// System.out.print(catalanMem(n,dp));
 		// System.out.print(catalanTab(n));
 	}
+
+	public static int mcm(int[] matrixces,int i,int j)
+	{
+		if(i==j) return 0;
+
+		int ans = Integer.MAX_VALUE;
+		for(int k = i; k<=j-1; k++)
+		{
+			int cost1 = mcm(matrixces,i,k);
+			int cost2 = mcm(matrixces,k+1,j);
+			int cost3 = matrixces[i-1]*matrixces[k]*matrixces[j];
+			int finalCost = cost1 + cost2 + cost3;
+
+			ans = Math.min(finalCost,ans); 
+		}
+
+		return ans;
+	}
+	public static void matrixChainMultiplication()
+	{
+		int matrixces[] = {1,2,3,4,3};
+		int n = matrixces.length;
+		System.out.println(mcm(matrixces,1,n-1));
+	}
 	public static void main(String args[])
 	{
 		// int n;
@@ -670,7 +694,8 @@ public class DynamicProgramming
 		// wildCardMatching();
 
 		// catalanNumber();
-		System.out.println(coutBST(4));
-		System.out.println(mountainRanges(4));
+		// System.out.println(coutBST(4));
+		// System.out.println(mountainRanges(4));
+		matrixChainMultiplication();
 	}
 }
