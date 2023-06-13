46b1bb8a9225f9609d87295f45fe27e78a9b932d
Rahul <88571832+Rahulwarkade@users.noreply.github.com>
2023-06-13 12:19:42 +0530

    Catalan numbers

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -533,6 +533,33 @@ public class DynamicProgramming
 
 		System.out.println(wildCardMatchingTab(text,pattern,n,m));
 	}
+
+	public static int catalanRec(int n,int[] dp)
+	{
+		if(n==0 || n==1)
+		{
+			return 1;
+		}
+
+		if(dp[n]!=-1)
+			return dp[n];
+
+		int ans = 0;
+
+		for(int i=0; i<=n-1; i++)
+		{
+			ans += catalanRec(i,dp) * catalanRec(n-i-1,dp);
+		}
+
+		return dp[n] = ans;
+	}
+	public static void catalanNumber()
+	{
+		int n = 12;
+		int[] dp = new int[n+1];
+		Arrays.fill(dp,-1);
+		System.out.print(catalanRec(n,dp));
+	}
 	public static void main(String args[])
 	{
 		// int n;
@@ -571,6 +598,8 @@ public class DynamicProgramming
 
 		// editDistance("intention","execution");
 
-		wildCardMatching();
+		// wildCardMatching();
+
+		catalanNumber();
 	}
 }
