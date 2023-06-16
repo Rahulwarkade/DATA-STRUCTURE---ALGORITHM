96f77a7009f0074fa13cc2603a75cf24164e0a8b
Rahul <88571832+Rahulwarkade@users.noreply.github.com>
2023-06-16 09:33:44 +0530

    build query and update on segment tree for min value

--- Segment_Tree.java
+++ Segment_Tree.java
@@ -87,6 +87,20 @@ class Segment_Tree
 
 		st[i] = Math.max(st[2*i+1],st[2*i+2]);
 	}
+	public static void buildST3(int[] arr,int i,int si,int sj)
+	{
+		if(si==sj)
+		{
+			st[i] = arr[si];
+			return;
+		}
+
+		int mid = si+(sj-si)/2;
+		buildST2(arr,2*i+1,si,mid);
+		buildST2(arr,2*i+2,mid+1,sj);
+
+		st[i] = Math.min(st[2*i+1],st[2*i+2]);
+	}
 
 	public static int getMaxUtil(int i,int si,int sj,int qi,int qj)
 	{
@@ -114,18 +128,99 @@ class Segment_Tree
 
 		System.out.println(getMaxUtil(0,0,n-1,qi,qj));
 	}
+
+	public static int getMinUtil(int i,int si,int sj,int qi,int qj)
+	{
+		if(qi>sj || qj<si)
+		{
+			return Integer.MAX_VALUE;
+		}
+		else if(qi<=si && sj<=qj)
+		{
+			return st[i];
+		}
+		else 
+		{
+			int mid = si + (sj-si)/2;
+
+			int leftmax = getMinUtil(2*i+1,si,mid,qi,qj);
+			int rightmax = getMinUtil(2*i+2,mid+1,sj,qi,qj);
+
+			return Math.min(leftmax,rightmax);
+		}
+	}
+	public static void getMin(int[] arr,int qi,int qj)
+	{
+		int n = arr.length;
+
+		System.out.println(getMinUtil(0,0,n-1,qi,qj));
+	}
+
+	public static void updateMaxUtil(int i,int si,int sj,int idx,int newValue)
+	{
+		if(idx<si || idx>sj)
+		{
+			return;
+		}
+		else if(si==sj)
+		{
+			st[i] = newValue;
+		}
+		else if(si!=sj)
+		{	
+			st[i] = Math.max(st[i],newValue);
+			int mid = si+(sj-si)/2;
+			updateMaxUtil(2*i+1,si,mid,idx,newValue);
+			updateMaxUtil(2*i+2,mid+1,sj,idx,newValue);
+		}
+	}
+	public static void updateMax(int[] arr,int idx,int value)
+	{
+		int n = arr.length;
+		arr[idx] = value;
+
+		updateMaxUtil(0,0,n-1,idx,value);
+	}
+	public static void updateMinUtil(int i,int si,int sj,int idx,int newValue)
+	{
+		if(idx<si || idx>sj)
+		{
+			return;
+		}
+		else if(si==sj)
+		{
+			st[i] = newValue;
+		}
+		else if(si!=sj)
+		{	
+			st[i] = Math.min(st[i],newValue);
+			int mid = si+(sj-si)/2;
+			updateMinUtil(2*i+1,si,mid,idx,newValue);
+			updateMinUtil(2*i+2,mid+1,sj,idx,newValue);
+		}
+	}
+	public static void updateMin(int[] arr,int idx,int value)
+	{
+		int n = arr.length;
+		arr[idx] = value;
+
+		updateMinUtil(0,0,n-1,idx,value);
+	}
 	public static void segmentTree(int[] arr,int n)
 	{
 		init(n);
 		// buildST(arr,st,0,0,n-1);
 
-		buildST2(arr,0,0,n-1);
-		// for(int i=0; i<n*2; i++)
-		// {
-		// 	System.out.print(st[i]+" ");
-		// }
+		buildST3(arr,0,0,n-1);
 
-		getMax(arr,2,5);
+		// getMax(arr,2,5);
+		// updateMax(arr,2,20);
+		// getMax(arr,2,5);	
+
+		getMin(arr,2,5);
+		updateMin(arr,2,20);
+		getMin(arr,2,5);
+
 		// System.out.println(getSum(arr,2,5));
 		// update(arr,2,2);
 		// System.out.println(getSum(arr,2,5));
